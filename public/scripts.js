e32c62dcdb2b470eadb6cf1760268747d2cc88ce
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-05-02 11:39:53 -0300

    finished

--- public/scripts.js
+++ public/scripts.js
@@ -10,3 +10,61 @@ for (item of menuItems) {
 if (currentPage.includes("members")) {
     document.body.style.backgroundImage = "url(https://images.pexels.com/photos/864939/pexels-photo-864939.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)"
 }
+
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        if (oldPage && currentPage - oldPage > 2) {
+            pages.push("...")
+        }
+        
+        if (oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        } 
+        
+        pages.push(currentPage)
+        
+        oldPage = currentPage
+    }
+    }
+
+    return pages
+}
+
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+
+    let elements = ""
+
+    for (let page of pages) {
+        if (String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+            elements += `<a href="?page=${page}">${page}</a>`
+        }
+        } 
+    }
+
+    pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+
+
+if (pagination) {
+    createPagination(pagination)
+}
--- src/app/models/instructors.js
+++ src/app/models/instructors.js
@@ -95,5 +95,43 @@ module.exports = {
 
             return callback()
         })
+    },
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(SELECT count(*) FROM instructors) AS total` 
+
+        if (filter) {
+            filterQuery = `
+            WHERE instructors.name ILIKE '%${filter}%'
+            OR instructors.modalities ILIKE '%${filter}%'
+            OR instructors.period ILIKE '%${filter}%'
+            `
+
+            totalQuery = `(
+                SELECT count(*) FROM instructors
+                ${filterQuery}
+            ) AS total`
+
+        }
+
+        query = ` 
+        SELECT instructors.*,
+        ${totalQuery},
+        count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        ${filterQuery}
+        GROUP BY instructors.id LIMIT $1 OFFSET $2
+        `
+
+        db.query(query, [limit, offset], function (err, results) {
+            if (err) throw "Data base ERROR"
+
+            callback(results.rows)
+        })
+
     }
 }
